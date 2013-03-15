0bdf86b35d203a74428d4d5b6af2674cf87a3120
kamilski81 <kamilski81@gmail.com>
2013-03-14 19:16:05 -0500

    add sprockets

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require 'action_controller/railtie'
 require 'action_mailer/railtie'
 require 'active_resource/railtie'
+require 'sprockets/railtie'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -68,5 +69,7 @@ module SocialcrunchSmk
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.initialize_on_precompile = false
   end
 end
