d471697e4356fba0a85d6e800fd9f28470b2e8ba
kamilski81 <kamilski81@gmail.com>
2013-03-20 11:59:53 -0500

    unicorn for newrelic

---
+++ config/initializers/unicorn.rb
@@ -0,0 +1,5 @@
+# https://newrelic.com/docs/ruby/no-data-with-unicorn
+# Ensure the agent is started using Unicorn
+# This is needed when using Unicorn and preload_app is not set to true.
+# See http://support.newrelic.com/kb/troubleshooting/unicorn-no-data
+::NewRelic::Agent.after_fork(:force_reconnect => true) if defined? Unicorn
